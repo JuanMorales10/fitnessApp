09fbfa54c296a743454362cb23ffcf7a023d0835
Dev-juan <juanmoralesp19@gmail.com>
2024-08-11 18:31:15 +0200

    Modificando vista crear comida

--- back/routes/openai.js
+++ back/routes/openai.js
@@ -13,33 +13,33 @@ router.post('/generate', async (req, res) => {
     console.log('Received prompt:', prompt);
     console.log('Using API Key:', OPENAI_API_KEY);
 
-    const makeRequest = async (retries = 0, delay = initialDelay) => {
-        try {
-            const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-                model: 'gpt-4-turbo', 
-                messages: [{ role: 'user', content: prompt }],
-                max_tokens: 800
-            }, {
-                headers: {
-                    'Authorization': `Bearer ${OPENAI_API_KEY}`,
-                    'Content-Type': 'application/json'
-                }
-            });
+    // const makeRequest = async (retries = 0, delay = initialDelay) => {
+    //     try {
+    //         const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    //             model: 'gpt-4-turbo', 
+    //             messages: [{ role: 'user', content: prompt }],
+    //             max_tokens: 800
+    //         }, {
+    //             headers: {
+    //                 'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         });
 
-            return response.data;
-        } catch (error) {
-            if (error.response) {
-                console.log(`Error response from OpenAI API:`, error.response.data);
-                if (error.response.status === 429 && retries < maxRetries) {
-                    console.log(`Too many requests, retrying after a delay of ${delay / 1000} seconds...`);
-                    await new Promise(resolve => setTimeout(resolve, delay));
-                    return makeRequest(retries + 1, delay * 2);
-                }
-            } else {
-                console.error('Error connecting to OpenAI API:', error.message);
-            }
-            throw error;
-        }
+    //         return response.data;
+    //     } catch (error) {
+    //         if (error.response) {
+    //             console.log(`Error response from OpenAI API:`, error.response.data);
+    //             if (error.response.status === 429 && retries < maxRetries) {
+    //                 console.log(`Too many requests, retrying after a delay of ${delay / 1000} seconds...`);
+    //                 await new Promise(resolve => setTimeout(resolve, delay));
+    //                 return makeRequest(retries + 1, delay * 2);
+    //             }
+    //         } else {
+    //             console.error('Error connecting to OpenAI API:', error.message);
+    //         }
+    //         throw error;
+    //     }
     };
 
     try {
